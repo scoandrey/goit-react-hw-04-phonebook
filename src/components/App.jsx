302481666901425bd52b50764c22203038a4e17d
Andrii <scoandrey@gmail.com>
2023-08-08 10:39:18 -0400

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import React from 'react';
 import { nanoid } from 'nanoid';
 import Contacts from 'components/Contacts/Contacts';
